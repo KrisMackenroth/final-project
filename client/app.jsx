29432ef8f09dd463202f2d03ef69f460813bc9ea
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-07-27 02:34:50 +0000

    Added navbar.

--- client/app.jsx
+++ client/app.jsx
@@ -4,6 +4,7 @@ import { parseRoute } from './lib';
 import Auth from './pages/auth';
 import AppContext from './lib/app-context';
 import jwtDecode from 'jwt-decode';
+import TemporaryDrawer from './components/navbar';
 
 export default class App extends React.Component {
 
@@ -12,8 +13,10 @@ export default class App extends React.Component {
     this.state = {
       user: null,
       isAuthorizing: true,
-      route: parseRoute(window.location.hash)
+      route: parseRoute(window.location.hash),
+      isClicked: false
     };
+    this.handleClick = this.handleClick.bind(this);
     this.handleSignIn = this.handleSignIn.bind(this);
   }
 
@@ -26,6 +29,10 @@ export default class App extends React.Component {
     const token = window.localStorage.getItem('react-context-jwt');
     const user = token ? jwtDecode(token) : null;
     this.setState({ user, isAuthorizing: false });
+  }
+
+  handleClick() {
+
   }
 
   handleSignIn(result) {
@@ -49,14 +56,22 @@ export default class App extends React.Component {
     const { user, route } = this.state;
     const { handleSignIn, handleSignOut } = this;
     const contextValue = { user, route, handleSignIn, handleSignOut };
+    const best = <TemporaryDrawer />;
     return (
       <AppContext.Provider value={contextValue}>
       <React.Fragment>
+
         <nav className="navbar background-light-grey">
           <div className="container-fluid justify-content-center">
-            <span className="navbar-brand mb-0 h1 karla-medium-italic fs-3">MyWorkout</span>
+              <div className='col'>{best}</div>
+              <div className='col text-center margin-0'>
+              <span className="mb-0 h1 karla-medium-italic fs-3">MyWorkout</span>
+              </div>
+              <div className='col'></div>
           </div>
+
         </nav>
+
         <div className='container-fluid background-dark-blue'>
       <div className='col'>
       { this.renderPage() }
---
+++ client/components/navbar.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+
+  const toggleDrawer = (anchor, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = anchor => (
+    <Box
+      className='background-dark-blue height'
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <nav className="navbar background-light-grey">
+        <div className="container-fluid justify-content-center">
+          <span className="navbar-brand mb-0 h1 karla-medium-italic fs-3">Menu</span>
+        </div>
+      </nav>
+      <List>
+        {['My Profile', 'Macros', 'Workouts', 'Sign Out'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      {['left'].map(anchor => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}><i className="fa-solid fa-bars big"></i></Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
